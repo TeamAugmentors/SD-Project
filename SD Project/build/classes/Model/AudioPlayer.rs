b702c6def87733b95d246c797fdadbd3f2e69f3f
AhmedSaadTanim <astdragneel@yahoo.com>
2020-03-17 22:03:29 +0600

    Intro and Login Screen

---
+++ SD Project/build/classes/Model/AudioPlayer.rs
@@ -0,0 +1,2 @@
+application.AudioPlayer$Status
+application.AudioPlayer
---
+++ SD Project/src/sd/project/SDProject.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd.project;
+
+import JFrame.SmartAssistant;
+import application.TextToSpeech;
+import marytts.modules.synthesis.Voice;
+
+/**
+ *
+ * @author AST
+ */
+public class SDProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        SmartAssistant sm = new SmartAssistant();
+        sm.setVisible(true);
+        
+        
+        TextToSpeech tts = new TextToSpeech();
+        
+        //printing available voices
+        //Voice.getAvailableVoices().stream().forEach(System.out::println);
+        
+        //tts.setVoice("cmu-slt-hsmm");
+        //tts.speak("Welcome to the Application! Please click the button below to proceed.", 1.0f, false, false);
+    }
+    
+}
